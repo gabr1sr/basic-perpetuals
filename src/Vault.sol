1cada18e25e8e1a839c45a68c533ca43ab9a878a
Gabriel Rosa <gabr1sr@proton.me>
2023-11-29 08:28:05 -0300

    add: ERC4626 Vault, Vault Tests, and ERC20 Mock
    Related to #15

---
+++ src/Vault.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {ERC4626} from "solady/src/tokens/ERC4626.sol";
+import {FixedPointMathLib} from "solady/src/utils/FixedPointMathLib.sol";
+
+contract Vault is ERC4626 {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         CONSTANTS                          */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    uint256 internal constant _MAX_UTILIZATION_RATE = 80;
+
+    address internal immutable _underlying;
+    
+    uint8 internal immutable _decimals;
+
+    string internal _name;
+    
+    string internal _symbol;
+
+    bool public immutable useVirtualShares;
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       CONSTRUCTOR                          */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    
+    constructor(address underlying_, string memory name_, string memory symbol_, bool useVirtualShares_) {
+	_underlying = underlying_;
+	
+	(bool success, uint8 result) = _tryGetAssetDecimals(underlying_);
+	_decimals = success ? result : _DEFAULT_UNDERLYING_DECIMALS;
+
+	_name = name_;
+	_symbol = symbol_;
+
+	useVirtualShares = useVirtualShares_;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       ERC20 METADATA                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    
+    function name() public view override returns (string memory) {
+	return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+	return _symbol;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     ERC4626 CONSTANTS                      */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    
+    function asset() public view override returns (address) {
+	return _underlying;
+    }
+
+    function _underlyingDecimals() internal view override returns (uint8) {
+	return _decimals;
+    }
+
+    function _useVirtualShares() internal view override returns (bool) {
+	return useVirtualShares;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         PERPETUALS                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function maxUtilization() public view returns (uint256 assets) {
+	assets = FixedPointMathLib.fullMulDiv(totalAssets(), _MAX_UTILIZATION_RATE, 100);
+    }
+}
---
+++ test/Vault.t.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+import {Vault} from "../src/Vault.sol";
+import {ERC20Mock} from "./mocks/ERC20Mock.sol";
+
+contract VaultTest is Test {
+    ERC20Mock public token;
+    Vault public vault;
+
+    address public alice = makeAddr("alice");
+    address public bob = makeAddr("bob");
+    address public charlie = makeAddr("charlie");
+    
+    function setUp() public {
+	// Underlying token
+	token = new ERC20Mock("USDC Mock", "USDC", 8);
+
+	// Vault
+	vault = new Vault(address(token), "USDC/BTC Liquidity Pool", "lpUSDCBTC", false);
+    }
+
+    function _mintUnderlying(address to, uint256 amount) internal {
+	// Mint `amount` of USDC for `to` address
+	token.mint(to, amount);
+
+	// Assert `to` address USDC balance
+	assertEq(token.balanceOf(to), amount);
+    }
+
+    function _deposit(uint256 amount, address to) internal {
+	// Impersonates `to` address
+	vm.startPrank(to);
+
+	// Shares balance before depositing
+	uint256 sharesBalanceBefore = vault.balanceOf(to);
+
+	// Assets balance before depositing
+	uint256 assetsBalanceBefore = token.balanceOf(to);
+
+	// Expected amount of shares to be received
+	uint256 expectedShares = vault.previewDeposit(amount);
+	
+	// Allow Vault to transfer `amount` of USDC from `to` address
+	token.approve(address(vault), amount);
+
+	// Add `amount` of USDC to the Vault
+	vault.deposit(amount, to);
+
+	// Assert expected amount of assets
+	assertEq(token.balanceOf(to), assetsBalanceBefore - amount);
+	
+	// Assert expected amount of shares
+	assertEq(vault.balanceOf(to), sharesBalanceBefore + expectedShares);
+
+	// Stops impersonating
+	vm.stopPrank();
+    }
+
+    function _withdraw(uint256 amount, address to) internal {
+	// Impersonates `to` address
+	vm.startPrank(to);
+
+	// Shares balance before withdraw
+	uint256 sharesBalanceBefore = vault.balanceOf(to);
+
+	// Assets balance before withdraw
+	uint256 assetsBalanceBefore = token.balanceOf(to);
+
+	// Expected amount of shares to send
+	uint256 expectedShares = vault.previewWithdraw(amount);
+
+	// Withdraw assets from Vault
+	vault.withdraw(amount, to, to);
+
+	// Assert expected amount of assets
+	assertEq(token.balanceOf(to), assetsBalanceBefore + amount);
+
+	// Assert expected amount of shares
+	assertEq(vault.balanceOf(to), sharesBalanceBefore - expectedShares);
+
+	// Stops impersonating
+	vm.stopPrank();
+    }
+
+    function testFuzz_SingleDepositWithdraw(uint256 amount) public {
+	// Mint underlying token
+	_mintUnderlying(alice, amount);
+
+	// Vault total assets before deposit
+	uint256 totalAssetsBefore = vault.totalAssets();
+
+	// Add `amount` of USDC to the Vault
+	_deposit(amount, alice);
+
+	// Assert Vault USDC balance
+	assertEq(vault.totalAssets(), amount);
+
+	// Withdraw `amount` of USDC from the Vault
+	_withdraw(amount, alice);
+
+	// Assert Vault USDC balance
+	assertEq(vault.totalAssets(), totalAssetsBefore);
+    }
+
+    function testFuzz_ThreeDepositWithdraw(uint256 amount) public {
+	// Limit the `amount` value while fuzzing to not overflow
+	vm.assume(amount <= type(uint256).max / 3);
+
+	// Mint `amount` of USDC for Alice, Bob, and Charlie
+        _mintUnderlying(alice, amount);
+	_mintUnderlying(bob, amount);
+	_mintUnderlying(charlie, amount);
+
+	// Vault total assets before deposit
+	uint256 totalAssetsBefore = vault.totalAssets();
+
+	// Deposit `amount` of USDC to the Vault as Alice, Bob, and Charlie
+	_deposit(amount, alice);
+	_deposit(amount, bob);
+	_deposit(amount, charlie);
+
+	// Assert Vault USDC balance
+	assertEq(vault.totalAssets(), amount * 3);
+
+	// Withdraw `amount` of USDC from the Vault as Alice, Bob, and Charlie
+	_withdraw(amount, alice);
+	_withdraw(amount, bob);
+	_withdraw(amount, charlie);
+
+	// Assert Vault USDC balance
+	assertEq(vault.totalAssets(), totalAssetsBefore);
+    }
+}
