f1d60ce6d454a649694093e70a1cc7544ff72462
Gabriel Rosa <gabr1sr@proton.me>
2024-01-07 23:45:17 -0300

    add: direnv support

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Basic Perpetuals";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix";
+  };
+
+  outputs = { self, nixpkgs, utils, foundry }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlay ];
+        };
+      in {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            foundry-bin
+            solc
+          ];
+
+          shellHook = ''
+            export PS1="[dev] $PS1"
+          '';
+        };
+    });
+}
